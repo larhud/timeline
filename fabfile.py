ba1f509abb79e3a4a8901156885334bdcd6dadd2
josircg <josircg@gmail.com>
2022-06-28 18:16:54 -0300

    Layout Timeline azul

--- fabfile.py
+++ fabfile.py
@@ -1,24 +1,25 @@
-# Precisa digitar a chave privada do git em producao #
-# coding: utf-8
-from fabric import task, Connection
 from io import BytesIO
 from datetime import datetime
+from fabric import task, Connection
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 
 
 @task
 def deploy(context):
-    connection = Connection('200.130.45.79', user='webapp', port=8090)
-    with connection.cd('/var/webapp/vepeinfo/vepeinfo'):
+    connection = Connection('200.130.45.80', user='webapp', port=8090)
+    with connection.cd('/var/webapp/vepeinfo/timeline'):
         connection.run('git pull')
         connection.run('../bin/python manage.py migrate')
         connection.run('../bin/python manage.py collectstatic --noinput')
-        connection.run('supervisorctl restart vepeinfo')
+        connection.run('supervisorctl restart timeline')
         print('Atualização efetuada')
 
 
 @task
 def connect(context):
-    server = Connection('200.130.45.79', user='webapp', port=8090)
+    server = Connection('200.130.45.80', user='webapp', port=8090)
     with server.cd('/var/webapp/'):
         result = server.run('ls', hide=True)
     msg = "Ran {0.command!r} on {0.connection.host}, got stdout:\n{0.stdout}"
@@ -44,4 +45,4 @@ def backup(connection):
 
 @task
 def backup_producao(context):
-    backup(Connection('200.130.45.79', user='webapp', port=8090))
+    backup(Connection('200.130.45.80', user='webapp', port=8090))
