8c856590179c0a955ff91b743ae0a1d4c09c65e8
Fabio Ferreira da Silva <fabio_ferreiradasilva@yahoo.com.br>
2023-04-28 11:43:38 -0300

    Issue 30

--- base/admin.py
+++ base/admin.py
@@ -99,10 +99,17 @@ class NoticiaAdmin(PowerModelAdmin):
 
     def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
 
-        context.update({
-            'show_save_and_continue': False,
-            'show_save_and_add_another': False,
-        })
+        if add:
+            context.update({
+                'show_save': False,
+                'show_save_and_continue': self.has_add_permission(request),
+                'title': f'{self.model._meta.verbose_name} - Etapa 1'
+            })
+        else:
+            context['show_save_and_continue'] = False
+
+        context['show_save_and_add_another'] = False
+
         return super().render_change_form(request, context, add, change, form_url, obj)
 
     def formfield_for_dbfield(self, db_field, **kwargs):
